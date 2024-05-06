9e7483b100fab433eeb676ba7f38c7f75bb73d89
Mootassam <mootassame@gmail.com>
2024-05-06 23:01:58 +0530

    add the new commit

---
+++ assets/Amount-1793c2f3.js
@@ -0,0 +1,1 @@
+class t{static Dollar(r){return r?r.toLocaleString("en-US",{style:"currency",currency:"USD"}):"$0.00"}}export{t as A};
---
+++ assets/Team-d85ff154.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-101b4caa.js";import{S as r}from"./SubHeader-d1c73129.js";function i(){return e.jsxs("div",{children:[e.jsx(r,{title:"Team",path:"/profile"}),e.jsx("h1",{children:"Team"})]})}export{i as default};
